c381ed695e40d67ed9e3e52cadb3cb1594e4e9cc
Tobias Möhring <dasbente@gmail.com>
2017-12-05 01:09:23 +0100

    Add URL building for gelbooru URLs

--- src/booru_dler/core.clj
+++ src/booru_dler/core.clj
@@ -1,6 +1,40 @@
 (ns booru-dler.core
   (:gen-class))
 
+(defn parse-args
+  "Parses the arguments supplied to a query parameter"
+  [args]
+  (cond (string? args) args
+        (sequential? args) (clojure.string/join "+" args)
+        (coll? args) (parse-args (seq args))
+        :else (str args)))
+
+(defn php-query 
+  "Creates a PHP-style query URL"
+  ([page script query-vect]
+   (let [queries (clojure.string/join "&" 
+                                      (map (fn [[param val]] 
+                                             (str (name param) "=" (parse-args val)))
+                                           query-vect))]
+     (str page "/" script "?" queries)))
+  ([page query-vect] 
+   (php-query page "index.php" query-vect)))
+
+(defn gelbooru-urls
+  "Create a list of URLs to gelbooru from a list of given tags, page sizes and pages"
+  ([tags limit pages]
+   (map (fn [page]
+          (php-query "https://gelbooru.com"
+                     {:page "dapi"
+                      :s "post"
+                      :q "index"
+                      :tags tags
+                      :limit limit
+                      :pid (* (dec page) limit)}))
+        pages))
+  ([tags pages] 
+   (gelbooru-urls tags 100 pages)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
