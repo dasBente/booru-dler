394c7a93286755fa6b0deed2134da5591fff5d96
Tobias Möhring <dasbente@gmail.com>
2017-12-11 02:21:54 +0100

    Use clojure.xml instead of self-defined parsing

--- src/booru_dler/core.clj
+++ src/booru_dler/core.clj
@@ -1,5 +1,6 @@
 (ns booru-dler.core
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [clojure.xml :as xml])
   (:gen-class))
 
 (defn parse-args
@@ -21,89 +22,64 @@
   ([page query-vect] 
    (php-query page "index.php" query-vect)))
 
+(defn gelbooru-url
+  "Creates a single URL to gelbooru from a list of tags, a limit and a given page"
+  ([tags limit page]
+   (php-query "https://gelbooru.com"
+              {:page "dapi"
+               :s "post"
+               :q "index"
+               :tags (if (empty? tags) "all" tags)
+               :limit limit
+               :pid (* (dec page) limit)}))
+  ([tags page]
+   (gelbooru-url tags 100 page)))
+
 (defn gelbooru-urls
   "Create a list of URLs to gelbooru from a list of given tags, page sizes and pages"
   ([tags limit pages]
-   (map (fn [page]
-          (php-query "https://gelbooru.com"
-                     {:page "dapi"
-                      :s "post"
-                      :q "index"
-                      :tags tags
-                      :limit limit
-                      :pid (* (dec page) limit)}))
-        pages))
+   (map (partial gelbooru-url tags limit) pages))
   ([tags pages] 
    (gelbooru-urls tags 100 pages)))
+
+(defn pparse
+  "Applies clojure.xml/parse in parallel to a list of URLs"
+  [urls]
+  (pmap xml/parse urls))
+
+(defn extract-image-url
+  "Retrieves all URLs to full size images from a pre-parsed gelbooru XML document"
+  [xml]
+  (map (fn [post] (:file_url (:attrs post))) (:content xml)))
+
+(defn query-to-urls
+  "Parses a query to gelbooru into a list of images"
+  ([tags limit pages]
+   (-> (gelbooru-urls tags limit pages)
+       (pparse)
+       (first)
+       (extract-image-url)
+       (flatten)))
+  ([tags pages] 
+   (query-to-urls tags 100 pages)))
+
+(defn non-concurrent
+  [tags pages]
+  (-> (gelbooru-urls tags 100 pages)
+      ((partial map xml/parse))
+      (first)
+      (extract-image-url)
+      (flatten)))
 
 (defn download-from
   "Download from a list of urls"
   [urls]
   (pmap slurp urls))
 
-(defn next-element
-  "Breakes the next element (either text or a opening or closing tag) from a xml document"
-  [xml-string]
-  (if (= "<" (subs xml-string 0 1))
-    (let [[tag rest] (str/split xml-string #">" 2)]
-      [(str tag ">") rest])
-    (let [[tag rest] (str/split xml-string #"<" 2)]
-      [tag (str "<" rest)])))
-
-(defn element-split
-  "Split a XML doc into elements (opening/closing tags and body strings)"
-  [xml-string]
-  (loop [res []
-         rem xml-string]
-    (if (empty? rem)
-      res
-      (let [[element rest] (next-element rem)]
-        (recur (conj res element) rest)))))
-
-(defn unwrap
-  "Removes the first and last character of a string"
-  [string]
-  (subs string 1 (dec (count string))))
-
-(defn opening-tag?
-  "Check if a tag is a opening tag"
-  [tag]
-  (= "<" (subs tag 0 1)))
-
-(defn empty-tag?
-  "Determines whether a tag is a empty tag"
-  [tag]
-  (let [len (count tag)]
-    (and (> 2 len)
-         (opening-tag? tag) 
-         (= "/>" (subs tag (- len 2))))))
-
-(defn closing-tag?
-  "Check if a tag is a closing tag"
-  [tag]
-  (= "</" (subs tag 2)))
-
-(defn tag-info
-  "Parses a tag and creates a vector of the form [tag-name {:arg val ...}]"
-  [tag]
-  (let [[name & attributes] (str/split (unwrap tag) #" ")]
-    (loop [rem attributes
-           res {}]
-      (if (empty? rem)
-        [name res]
-        (let [[curr & next] rem
-              [attr-name attr-val] (str/split curr #"=")]
-          (recur next (assoc res (keyword attr-name) (unwrap attr-val))))))))
-
-(defn get-tag-name
-  "Returns the name of a given XML tag as a key argument"
-  [tag]
-  (take-while #(not= % " ") (subs tag 1)))
-
-(defn parse-xml
-  [doc]
-  (let [tags (element-split doc)]
-    tags))
+(defn dev-null
+  "Discards any args, can be used to suppress return values"
+  [& args]
+  nil)
 
 (defn -main
   "I don't do a whole lot ... yet."
